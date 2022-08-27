61adec99674d7b6001232ec97bfb6c00608c1228
AndrewHanasiro <andrewkhanasiro@gmail.com>
2022-08-27 18:54:07 -0300

    feat: add prometheis

--- api/http/middlewares/metric.go
+++ api/http/middlewares/metric.go
@@ -1,45 +1,43 @@
 package middlewares
 
 import (
-	config "auth-plus-notification/config"
-	"fmt"
-	"log"
 	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/push"
 )
 
-func Logger() gin.HandlerFunc {
-	env := config.GetEnv()
+func Metric() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		t := time.Now()
-
-		// Set example variable
-		c.Set("example", "12345")
 
 		// before request
 		c.Next()
 
 		// after request
-		latency := time.Since(t)
-		log.Print(latency)
+		latency := float64(time.Since(t))
 
 		// access the status we are sending
 		status := c.Writer.Status()
-		log.Println(status)
 
 		completionTime := prometheus.NewGauge(prometheus.GaugeOpts{
 			Name: "request_latency",
 			Help: "Gauge request latency",
 		})
-		completionTime.SetToCurrentTime()
-		if err := push.New(env.Prometheus.Url+":"+env.Prometheus.Port, "db_backup").
-			Collector(completionTime).
-			Grouping("db", "customers").
-			Push(); err != nil {
-			fmt.Println("Could not push completion time to Pushgateway:", err)
+		errorCounter := prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "request_latency",
+			Help: "Gauge request latency",
+		})
+		succeedCounter := prometheus.NewCounter(prometheus.CounterOpts{
+			Name: "request_latency",
+			Help: "Gauge request latency",
+		})
+
+		completionTime.Set(latency)
+		if status >= 500 {
+			errorCounter.Inc()
+		} else {
+			succeedCounter.Inc()
 		}
 	}
 }
